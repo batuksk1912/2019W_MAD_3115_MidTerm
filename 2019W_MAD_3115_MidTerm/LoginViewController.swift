4d5db99f615dbd0fcff73b48ee7e6ffa2cbc2839
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-03-20 18:55:13 -0400

    Login screen design and code finished.

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -14,13 +14,41 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var studentPassword: UITextField!
     @IBOutlet weak var rememberMe: UISwitch!
     
+    let tempStudentId : String = "admin"
+    let tempStudentPassword : String = "admin@123"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let userDefault = UserDefaults.standard
+        if let id = userDefault.string(forKey: "studentId") {
+            studentId.text = id
+        }
+        if let psw = userDefault.string(forKey: "password") {
+            studentPassword.text = psw
+            rememberMe.isOn = true
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func loginButton(_ sender: UIButton) {
-        
+        if (studentId.text == tempStudentId && studentPassword.text == tempStudentPassword) {
+            let userDefault = UserDefaults.standard
+            if (rememberMe.isOn) {
+                userDefault.set(tempStudentId, forKey: "studentId")
+                userDefault.set(tempStudentPassword, forKey: "password")
+            } else {
+                userDefault.removeObject(forKey: "studentId")
+                userDefault.removeObject(forKey: "password")
+            }
+            print("Logged In")
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            let mainNC = sb.instantiateViewController(withIdentifier: "mainNC") as! MainNavigationController
+            present(mainNC,animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Student ID or Password Wrong!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
     }
     
 
---
+++ 2019W_MAD_3115_MidTerm/MainNavigationController.swift
@@ -0,0 +1,26 @@
+//
+//  MainNavigationController.swift
+//  2019W_MAD_3115_MidTerm
+//
+//  Created by Baturay Kayatürk on 2019-03-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
